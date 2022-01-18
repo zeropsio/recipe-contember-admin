1db8b03f4b5e780617618af4f3f6be56d042772d
Ales Rechtorik <foxxcz@gmail.com>
2022-01-18 21:24:04 +0100

    migration test

---
+++ admin/pages/Articles.tsx
@@ -0,0 +1,49 @@
+import {
+  TablePage,
+  TableCell,
+  Field,
+  CreatePage,
+  TextField,
+  TextAreaField,
+  PageLinkById,
+  EditPage
+} from "@contember/admin";
+
+export const ArticleList = (
+  <TablePage entities="Article" pageName="articles">
+    <TableCell>
+      <Field field="title" />
+    </TableCell>
+    <TableCell shrunk>
+      <PageLinkById to="article">edit</PageLinkById>
+    </TableCell>
+  </TablePage>
+);
+
+export const ArticleCreate = (
+  <CreatePage
+    entity="Article"
+    pageName="articleNew"
+    rendererProps={{ title: "Add a article" }}
+    redirectOnSuccess={(request, id) => ({
+      ...request,
+      pageName: "article",
+      parameters: {
+        id,
+      },
+    })}
+  >
+    <TextField field="title" label="Title" />
+    <TextAreaField field="content" label="content" />
+  </CreatePage>
+);
+
+export const ArticleEdit = (
+  <EditPage
+    entity="Article(id = $id)"
+    pageName="article"
+  >
+      <TextField field="title" label="Title" />
+      <TextAreaField field="content" label="content" />
+  </EditPage>
+)
