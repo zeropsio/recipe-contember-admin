e00e21d235d96b8673ae6db66e34bd473d5338c9
Ales Rechtorik <foxxcz@gmail.com>
2022-01-18 21:54:04 +0100

    migration test

--- admin/components/Navigation.tsx
+++ admin/components/Navigation.tsx
@@ -4,7 +4,7 @@ import { Menu } from '@contember/admin'
 export const Navigation = () => (
 	<Menu>
 		<Menu.Item>
-			<Menu.Item title="Dashboard" to="index" />
+			<Menu.Item title="Dashboard!" to="index" />
 			<Menu.Item title="Articles" to="articles" />
 			<Menu.Item title="Create new article" to="articleNew" />
 		</Menu.Item>
